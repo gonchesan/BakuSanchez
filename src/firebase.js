6defaf7a26503a22ea952d4cf831ac6972ccad57
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2022-02-22 15:01:19 -0300

    Add environment configuration for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFh6T7PG55aohwp0C68wg8DLeaQlyCJYU",
-  authDomain: "baku-store.firebaseapp.com",
-  projectId: "baku-store",
-  storageBucket: "baku-store.appspot.com",
-  messagingSenderId: "246842881908",
-  appId: "1:246842881908:web:05b74e2c16f6fda2459e21",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
